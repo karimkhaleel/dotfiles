afabe9603f33c62ed522286d0dea89b11e5a14fc
Karim Khaleel <code.karim@gmail.com>
2022-10-24 16:44:56 +0300

    Refactor plugins

--- dot_config/nvim/lua/plug.lua
+++ dot_config/nvim/lua/plug.lua
@@ -1,25 +1,22 @@
 return require('packer').startup(function(use)
     -- [[ Plugins go here ]]
     
-    -- LSP
-    use {"neovim/nvim-lspconfig"}
-
-    use {
-        "kyazdani42/nvim-tree.lua",
-        requires = "kyazdani42/nvim-web-devicons"
-    }
-
     -- [[ Theme ]]
     use { "mhinz/vim-startify" }
     use { "DanilaMihailov/beacon.nvim" }
     use {
         "nvim-lualine/lualine.nvim",
         requires = {"kyazdani42/nvim-web-devicons",
-                    opt = true}
+        opt = true}
     }
     use { "Mofiqul/dracula.nvim" }
-
+    
     -- [[ Dev ]]
+    use {"neovim/nvim-lspconfig"}                   -- language servers
+    use {                                           -- file explorer tab
+        "kyazdani42/nvim-tree.lua",
+        requires = "kyazdani42/nvim-web-devicons"
+    }
     use {
         "nvim-telescope/telescope.nvim",            -- fuzzy finder
         requires = { { "nvim-lua/plenary.nvim" } }
@@ -32,34 +29,34 @@ return require('packer').startup(function(use)
     use { "windwp/nvim-autopairs" }                 -- auto close brackets, etc.
     use { "mbbill/undotree" }                       -- undo history
     use { "tpope/vim-commentary" }                  -- comment hotkey
-    use {
+    use {                                           -- code highlighter
         "nvim-treesitter/nvim-treesitter",
         run = function() require("nvim-treesitter.install").update({ with_sync = true }) end,
     }
     use { "gennaro-tedesco/nvim-jqx" }              -- json prettifier
 
-    use {
-        "phaazon/hop.nvim",                         -- code navigation
+    use {                                           -- code navigation
+        "phaazon/hop.nvim",
         branch = "v2"
     }
     
-    use { 
-        "ms-jpq/coq_nvim",                          -- autocomplete
+    use {                                           -- autocomplete
+        "ms-jpq/coq_nvim",
         branch = "coq"
     }
-    use { 
-        "ms-jpq/coq.artifacts",                     -- autocomplete snippets
+    use {                                           -- autocomplete snippets
+        "ms-jpq/coq.artifacts",
         branch = "artifacts",
         requires = { "ms-jpg/coq_nvim" }
     }
-    use { 
-        "ms-jpq/coq.thirdparty",                    -- autocomplete thirdparty tools
+    use {                                           -- autocomplete thirdparty tools
+        "ms-jpq/coq.thirdparty",
         branch = "3p",
         requires = { "ms-jpg/coq_nvim" }
 
     }
 
-    use {
+    use {                                           -- code problems tab
         "folke/trouble.nvim",
         requires = "kyazdani42/nvim-web-devicons"
     }
