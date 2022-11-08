ad317074b17975e64a22b68608364bad762e28b5
Karim Khaleel <karim.khaleel@makane.com>
2022-11-08 14:34:07 +0300

    Neovim: Remove virtual text
    This change disables git blame as well. Need to come up with a
    solution for that.

--- dot_config/nvim/lua/opts.lua
+++ dot_config/nvim/lua/opts.lua
@@ -3,34 +3,28 @@ local cmd = vim.api.nvim_command
 local acmd = vim.api.nvim_create_autocmd
 
 -- [[ Context ]]
-opt.number = true -- bool: Show line numbers
-opt.relativenumber = true -- bool: Show relative line numbers
-opt.scrolloff = 4 -- int:  Min num lines of context
-opt.signcolumn = "yes" -- str:  Show the sign column
+opt.number = true                                   -- bool: Show line numbers
+opt.relativenumber = true                           -- bool: Show relative line numbers
+opt.scrolloff = 4                                   -- int:  Min num lines of context
+opt.signcolumn = "yes"                              -- str:  Show the sign column
 
 -- [[ Filetypes ]]
-opt.encoding = "utf8" -- str:  String encoding to use
-opt.fileencoding = "utf8" -- str:  File encoding to use
+opt.encoding = "utf8"                               -- str:  String encoding to use
+opt.fileencoding = "utf8"                           -- str:  File encoding to use
 
 -- [[ Theme ]]
-opt.syntax = "ON" -- str:  Allow syntax highlighting
-opt.termguicolors = true -- bool: If term supports ui color then enable
-cmd[[colorscheme tokyonight]]
+opt.syntax = "ON"                                   -- str:  Allow syntax highlighting
+opt.termguicolors = true                            -- bool: If term supports ui color then enable
 
 -- [[ Whitespace ]]
-opt.expandtab = true -- bool: Use spaces instead of tabs
-opt.shiftwidth = 4 -- num:  Size of an indent
-opt.softtabstop = 4 -- num:  Number of spaces tabes count for in insert mode
-opt.tabstop = 4 -- num:  Number of spaces tabs count for
+opt.expandtab = true                                -- bool: Use spaces instead of tabs
+opt.shiftwidth = 4                                  -- num:  Size of an indent
+opt.softtabstop = 4                                 -- num:  Number of spaces tabes count for in insert mode
+opt.tabstop = 4                                     -- num:  Number of spaces tabs count for
 
 -- [[ Splits ]]
-opt.splitright = true -- bool: NPlace new window to the right of current one
-opt.splitbelow = true -- bool: Place new window below the current one
+opt.splitright = true                               -- bool: NPlace new window to the right of current one
+opt.splitbelow = true                               -- bool: Place new window below the current one
 
--- [[ Virtual Text ]]
-vim.diagnostic.config({
-    virtual_text = false
-})
-
--- [[ General Vim Settings ]]
-vim.g.updatetime = 100
+-- [[ LSP ]]
+vim.diagnostic.config({virtual_text = false})       -- bool: Stop showing virtual LSP diagnostic text
