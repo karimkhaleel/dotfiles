f005494f7084dcef4aa16030da77ba7069016f9b
Karim Khaleel <map[email:code.karim@gmail.com]>
2023-08-08 18:10:20 +0300

    feat(nvim): add nvchad config

---
+++ neovim/NvChad/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,32 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- if you just want default config for the servers then put them in a table
+local servers = { "html", "cssls", "tsserver", "clangd", "pyright" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+--
+lspconfig.pyright.setup {
+  on_attach = on_attach,
+  settings = {
+    pyright = { autoImportCompletion = true },
+    python = {
+      analysis = {
+        autoImportCompletions = true,
+        typeCheckingMode = "off",
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        diagnosticMode = "openFilesOnly",
+        stubPath = vim.fn.stdpath "data" .. "/lazy/python-type-stubs/stubs",
+      },
+    },
+  },
+}
