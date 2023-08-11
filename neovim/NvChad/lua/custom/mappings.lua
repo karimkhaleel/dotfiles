d6eee15b16ef1d1e0033d4ca79e67ca393e226f4
Karim Khaleel <map[email:code.karim@gmail.com]>
2023-08-11 16:15:11 +0300

    feat(nvim): add mappings to quit and save

--- neovim/NvChad/lua/custom/mappings.lua
+++ neovim/NvChad/lua/custom/mappings.lua
@@ -4,6 +4,8 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>qq"] = { ":q<enter>", "quit neovim", opts = { nowait = true } },
+    ["<leader>ww"] = { ":w<enter>", "save", opts = { nowait = true } },
   },
 }
 
