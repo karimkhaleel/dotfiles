349470cb8ab37ab1087c0405e2364e95444f7da8
Karim Khaleel <code.karim@gmail.com>
2023-10-02 21:09:17 +0300

    feat(neovim): go and rust

--- dot_config/nvim/lua/custom/configs/lspconfig.lua
+++ dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,55 +1,66 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
-local servers =
-  { "html", "cssls", "tsserver", "clangd", "pyright", "emmet_language_server", "astro", "lua_ls", "tailwindcss" }
+local servers = {
+	"html",
+	"cssls",
+	"tsserver",
+	"clangd",
+	"pyright",
+	"emmet_language_server",
+	"astro",
+	"lua_ls",
+	"tailwindcss",
+	"gopls",
+	"rust_analyzer",
+}
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
 
 --
-lspconfig.pyright.setup {
-  on_attach = on_attach,
-  settings = {
-    python = {
-      analysis = {
-        autoImportCompletions = true,
-        disableOrganizeImports = true,
-        typeCheckingMode = "off",
-        autoSearchPaths = true,
-        useLibraryCodeForTypes = true,
-        diagnosticMode = "openFilesOnly",
-        stubPath = vim.fn.stdpath "data" .. "/lazy/python-type-stubs/stubs",
-      },
-    },
-  },
-}
+lspconfig.pyright.setup({
+	on_attach = on_attach,
+	settings = {
+		python = {
+			analysis = {
+				autoImportCompletions = true,
+				disableOrganizeImports = true,
+				typeCheckingMode = "off",
+				autoSearchPaths = true,
+				useLibraryCodeForTypes = true,
+				diagnosticMode = "openFilesOnly",
+				stubPath = vim.fn.stdpath("data") .. "/lazy/python-type-stubs/stubs",
+			},
+		},
+	},
+})
 
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  init_options = {
-    preferences = {
-      disableSuggestions = true,
-    },
-  },
-}
+lspconfig.tsserver.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	init_options = {
+		preferences = {
+			disableSuggestions = true,
+		},
+	},
+})
 
-lspconfig.lua_ls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-    },
-  },
-}
+lspconfig.lua_ls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
+})
--- dot_config/nvim/lua/custom/configs/null-ls.lua
+++ dot_config/nvim/lua/custom/configs/null-ls.lua
@@ -1,41 +1,45 @@
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-local null_ls = require "null-ls"
+local null_ls = require("null-ls")
 
 local opts = {
-  sources = {
-    -- lua
-    null_ls.builtins.formatting.stylua,
+	sources = {
+		-- lua
+		null_ls.builtins.formatting.stylua,
 
-    -- js stuff
-    null_ls.builtins.diagnostics.eslint_d,
-    null_ls.builtins.formatting.prettier.with {
-      extra_filetypes = { "astro", "svelte" },
-    },
+		-- js stuff
+		null_ls.builtins.diagnostics.eslint_d,
+		null_ls.builtins.formatting.prettier.with({
+			extra_filetypes = { "astro", "svelte" },
+		}),
 
-    -- python sfuff
-    null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.isort.with { extra_args = { "--profile", "black" } },
-    null_ls.builtins.diagnostics.ruff,
-    null_ls.builtins.diagnostics.mypy.with { command = { "python", "-m", "mypy" } },
-  },
+		-- python sfuff
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.isort.with({ extra_args = { "--profile", "black" } }),
+		null_ls.builtins.diagnostics.ruff,
+		null_ls.builtins.diagnostics.mypy.with({ command = { "python", "-m", "mypy" } }),
 
-  on_attach = function(client, bufnr)
-    if client.supports_method "textDocument/formatting" then
-      vim.api.nvim_clear_autocmds {
-        group = augroup,
-        buffer = bufnr,
-      }
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          if require("custom.flags").format_on_save then
-            vim.lsp.buf.format { bufnr = bufnr }
-          end
-        end,
-      })
-    end
-  end,
+		-- go stuff
+		null_ls.builtins.formatting.gofumpt,
+		null_ls.builtins.diagnostics.golangci_lint,
+	},
+
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({
+				group = augroup,
+				buffer = bufnr,
+			})
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					if require("custom.flags").format_on_save then
+						vim.lsp.buf.format({ bufnr = bufnr })
+					end
+				end,
+			})
+		end
+	end,
 }
 
 return opts
