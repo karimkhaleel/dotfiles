eee8e0c518f0d12d5641bc1bb308ffea86783039
Karim Khaleel <code.karim@gmail.com>
2023-10-13 12:26:44 +0300

    feat(neovim): add surround plugin

--- dot_config/nvim/lua/custom/plugins.lua
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -42,6 +42,17 @@ local plugins = {
   },
 
   {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+
+  {
     "mfussenegger/nvim-dap-python",
     ft = "python",
     dependencies = {
