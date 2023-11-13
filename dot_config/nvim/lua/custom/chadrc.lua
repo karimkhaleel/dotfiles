86a5c730d77509a5683707e0d04d1b8e4a3abc09
Karim Khaleel <code.karim@gmail.com>
2023-11-13 15:45:44 +0300

    fix(neovim): specify transparency

--- dot_config/nvim/lua/custom/chadrc.lua
+++ dot_config/nvim/lua/custom/chadrc.lua
@@ -7,6 +7,7 @@ local highlights = require "custom.highlights"
 M.ui = {
   theme = "catppuccin",
   theme_toggle = { "catppuccin", "one_light" },
+  transparency = false,
 
   hl_override = highlights.override,
   hl_add = highlights.add,
