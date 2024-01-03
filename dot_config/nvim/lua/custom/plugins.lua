62f93557c445aa0a4c8613e0d4486baae485c66a
Karim Khaleel <code.karim@gmail.com>
2024-01-03 12:11:18 +0300

    feat(neovim): switch to none-ls

--- dot_config/nvim/lua/custom/plugins.lua
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -14,7 +14,7 @@ local plugins = {
   },
 
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     event = "VeryLazy",
     opts = function()
       return require "custom.configs.null-ls"
