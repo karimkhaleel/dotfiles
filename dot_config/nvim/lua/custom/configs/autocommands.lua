a8af5bb8d4d0ef9af3f81fb526a83430d62e53f6
Karim Khaleel <code.karim@gmail.com>
2024-01-09 12:53:02 +0300

    feat(neovim): add python venv selector

--- dot_config/nvim/lua/custom/configs/autocommands.lua
+++ dot_config/nvim/lua/custom/configs/autocommands.lua
@@ -29,3 +29,15 @@ vim.api.nvim_create_autocmd("TermEnter", {
     vim.cmd [[setlocal winhighlight=TermCursor:TerminalCursorShape]]
   end,
 })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  desc = "Auto select virtualenv Nvim open",
+  pattern = "*",
+  callback = function()
+    local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
+    if venv ~= "" then
+      require("venv-selector").retrieve_from_cache()
+    end
+  end,
+  once = true,
+})
