1bd6c92ddf7ed38cb8f9ac3a3e2743ee785a4201
Karim Khaleel <code.karim@gmail.com>
2024-01-10 14:08:49 +0300

    feat(neovim): add djlint for python

--- dot_config/nvim/lua/custom/configs/null-ls.lua
+++ dot_config/nvim/lua/custom/configs/null-ls.lua
@@ -1,90 +1,129 @@
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-local null_ls = require("null-ls")
+local null_ls = require "null-ls"
 
-local h = require("null-ls.helpers")
-local u = require("null-ls.utils")
-local methods = require("null-ls.methods")
+local h = require "null-ls.helpers"
+local u = require "null-ls.utils"
+local methods = require "null-ls.methods"
 
 local FORMATTING = methods.internal.FORMATTING
 
-local templ_fmt = h.make_builtin({
-	name = "templ_fmt",
-	meta = {
-		url = "templ.guide",
-		description = "Format templ",
-	},
-	method = FORMATTING,
-	filetypes = { "templ" },
-	generator_opts = {
-		command = "/home/karim/.local/share/nvim/mason/bin/templ",
-		args = {
-			"fmt",
-			"$FILENAME",
-		},
-		to_temp_file = true,
-	},
-	factory = h.formatter_factory,
-})
+local templ_fmt = h.make_builtin {
+  name = "templ_fmt",
+  meta = {
+    url = "templ.guide",
+    description = "Format templ",
+  },
+  method = FORMATTING,
+  filetypes = { "templ" },
+  generator_opts = {
+    command = "/home/karim/.local/share/nvim/mason/bin/templ",
+    args = {
+      "fmt",
+      "$FILENAME",
+    },
+    to_temp_file = true,
+  },
+  factory = h.formatter_factory,
+}
 
+-- Function to check if Poetry virtual environment exists
+-- local function get_poetry_venv_path()
+--   local handle = io.popen "poetry env info -p 2>/dev/null"
+--   local result = handle:read "*a"
+--   handle:close()
+--
+--   if result ~= "" then
+--     return result:gsub("\n", "")
+--   else
+--     return nil
+--   end
+-- end
+--
+-- local function get_venv_path()
+--   local handle = io.popen "echo $VIRTUAL_ENV"
+--   local result = handle:read "*a"
+--   handle:close()
+--
+--   if result ~= "" then
+--     return result:gsub("\n", "")
+--   else
+--     return nil
+--   end
+-- end
+--
+-- Configure mypy command based on Poetry environment
+-- local mypy_command = { "python", "-m", "mypy" }
+-- local poetry_venv_path = get_poetry_venv_path()
+-- local venv_path = get_venv_path()
+--
+-- if venv_path then
+--   mypy_command = { venv_path .. "/bin/python", "-m", "mypy" }
+-- elseif poetry_venv_path then
+--   mypy_command = { poetry_venv_path .. "/bin/python", "-m", "mypy" }
+-- else
+--   mypy_command = { "python", "-m", "mypy" }
+-- end
+--
 local opts = {
-	sources = {
-		-- lua
-		null_ls.builtins.formatting.stylua,
+  sources = {
+    -- lua
+    null_ls.builtins.formatting.stylua,
 
-		-- js stuff
-		null_ls.builtins.diagnostics.eslint_d,
-		null_ls.builtins.formatting.prettier.with({
-			extra_filetypes = { "astro", "svelte" },
-		}),
+    -- js stuff
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.formatting.prettier.with {
+      extra_filetypes = { "astro", "svelte" },
+    },
 
-		-- python sfuff
-		null_ls.builtins.diagnostics.ruff,
-		null_ls.builtins.formatting.ruff_format,
-		null_ls.builtins.formatting.isort.with({
-			extra_args = { "--profile", "black" },
-		}),
+    -- python sfuff
+    null_ls.builtins.diagnostics.ruff,
+    null_ls.builtins.formatting.djlint,
+    null_ls.builtins.formatting.ruff_format,
+    null_ls.builtins.formatting.isort.with {
+      extra_args = { "--profile", "black" },
+    },
 
-		null_ls.builtins.diagnostics.mypy,
+    null_ls.builtins.diagnostics.mypy,
 
-		-- C/C++ stuff
-		null_ls.builtins.formatting.clang_format,
+    -- C/C++ stuff
+    null_ls.builtins.formatting.clang_format,
 
-		-- go stuff
-		null_ls.builtins.formatting.gofumpt,
-		null_ls.builtins.diagnostics.golangci_lint,
-		templ_fmt,
+    -- go stuff
+    null_ls.builtins.formatting.gofumpt,
+    null_ls.builtins.diagnostics.golangci_lint,
+    templ_fmt,
 
-		-- nim stuff
-		null_ls.builtins.formatting.nimpretty,
+    -- nim stuff
+    null_ls.builtins.formatting.nimpretty,
 
-		-- rust stuff
-		null_ls.builtins.formatting.rustfmt,
+    -- rust stuff
+    null_ls.builtins.formatting.rustfmt,
 
-		-- sql stuff
-		null_ls.builtins.diagnostics.sqlfluff,
-		null_ls.builtins.formatting.sqlfluff,
+    -- sql stuff
+    null_ls.builtins.diagnostics.sqlfluff,
+    null_ls.builtins.formatting.sqlfluff,
 
-		-- zig stuff
-		null_ls.builtins.formatting.zigfmt,
-	},
+    -- zig stuff
+    null_ls.builtins.formatting.zigfmt,
+  },
 
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({
-				group = augroup,
-				buffer = bufnr,
-			})
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					if require("custom.flags").format_on_save then
-						vim.lsp.buf.format({ bufnr = bufnr })
-					end
-				end,
-			})
-		end
-	end,
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds {
+        group = augroup,
+        buffer = bufnr,
+      }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          if require("custom.flags").format_on_save then
+            vim.lsp.buf.format { bufnr = bufnr }
+          end
+        end,
+      })
+    end
+  end,
 }
 
 return opts
