969e6323641618c63e94ed11467630aeeccaca55
Karim Khaleel <code.karim@gmail.com>
2024-01-26 17:25:25 +0300

    feat(neovim): add jsonschema to json and yaml

--- dot_config/nvim/lua/custom/configs/lspconfig.lua
+++ dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -27,6 +27,30 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+lspconfig.jsonls.setup {
+  settings = {
+    json = {
+      schemas = require("schemastore").json.schemas(),
+      validate = { enable = true },
+    },
+  },
+}
+
+lspconfig.yamlls.setup {
+  settings = {
+    yaml = {
+      schemaStore = {
+        -- You must disable built-in schemaStore support if you want to use
+        -- this plugin and its advanced options like `ignore`.
+        enable = false,
+        -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+        url = "",
+      },
+      schemas = require("schemastore").yaml.schemas(),
+    },
+  },
+}
+
 local python_capabilities = {
   textDocument = {
     publishDiagnostics = {
