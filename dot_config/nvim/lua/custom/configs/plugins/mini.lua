89347dda6ebba3572e210e9be924c417edff4d01
Karim Khaleel <code.karim@gmail.com>
2024-03-09 16:48:20 +0300

    feat(neovim): add a couple textobject queries for mini ai

--- dot_config/nvim/lua/custom/configs/plugins/mini.lua
+++ dot_config/nvim/lua/custom/configs/plugins/mini.lua
@@ -1,6 +1,18 @@
 local mini_module_configs = {
   function()
-    require("mini.ai").setup()
+    local spec_treesitter = require("mini.ai").gen_spec.treesitter
+    require("mini.ai").setup {
+      custom_textobjects = {
+        l = spec_treesitter {
+          a = { "@conditional.outer", "@loop.outer" },
+          i = { "@conditional.inner", "@loop.inner" },
+        },
+        h = spec_treesitter {
+          a = { "@attribute.outer" },
+          i = { "@attribute.inner" },
+        },
+      },
+    }
   end,
   function()
     require("mini.files").setup()
