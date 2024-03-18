6cd985c59af4d7223961c186e4949e083436db3a
Karim Khaleel <code.karim@gmail.com>
2024-03-18 13:40:25 +0300

    fix(neovim): fix a bunch of stuff after nvchad 2.5 migration

--- dot_config/nvim/lua/chadrc.lua
+++ dot_config/nvim/lua/chadrc.lua
@@ -11,11 +11,35 @@ M.ui = {
 
   hl_override = highlights.override,
   hl_add = highlights.add,
+
+  tabufline = {
+    --  more opts
+    order = { "treeOffset", "buffers", "tabs", "btns" },
+    modules = {},
+  },
+
+  statusline = {
+    --  more opts
+    order = { "mode", "f", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
+    --  more modules
+    modules = {
+      f = function()
+        local icon = "󰈚"
+        local name = vim.fn.expand "%:p"
+
+        if name ~= "Empty " then
+          local devicons_present, devicons = pcall(require, "nvim-web-devicons")
+
+          if devicons_present then
+            local ft_icon = devicons.get_icon(name)
+            icon = (ft_icon ~= nil and ft_icon) or icon
+          end
+        end
+
+        return "%#St_file# " .. name
+      end,
+    },
+  },
 }
-
--- M.plugins = "plugins"
-
--- check core.mappings for table structure
--- M.mappings = require "mappings"
 
 return M
