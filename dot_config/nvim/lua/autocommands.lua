7f50d1965b056f315e49834f392dc7f2ae158647
Karim Khaleel <code.karim@gmail.com>
2024-06-21 21:32:37 +0300

    chore(neovim): update venv selector stuff

--- dot_config/nvim/lua/autocommands.lua
+++ dot_config/nvim/lua/autocommands.lua
@@ -30,18 +30,6 @@ vim.api.nvim_create_autocmd("TermEnter", {
   end,
 })
 
-vim.api.nvim_create_autocmd("VimEnter", {
-  desc = "Auto select virtualenv Nvim open",
-  pattern = "*",
-  callback = function()
-    local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
-    if venv ~= "" then
-      require("venv-selector").retrieve_from_cache()
-    end
-  end,
-  once = true,
-})
-
 -- Detect djangohtml filetype
 local function set_django_html_filetype()
   local path = vim.fn.expand "%:p"
--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -10,7 +10,7 @@ return {
     "linux-cultist/venv-selector.nvim",
     dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
     opts = {},
-    event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+    lazy = false,
     branch = "regexp",
     keys = {
       -- Keymap to open VenvSelector to pick a venv.
