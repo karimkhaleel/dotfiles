632e82d00d4b108b61dec6c0f2380bb18bbdd5c7
Karim Khaleel <code.karim@gmail.com>
2024-07-07 16:24:09 +0300

    fix(neovim): change mini.ai loop and conditional keybind

--- dot_config/nvim/lua/configs/mini.lua
+++ dot_config/nvim/lua/configs/mini.lua
@@ -1,18 +1,18 @@
 local M = {
   function()
     local spec_treesitter = require("mini.ai").gen_spec.treesitter
-    require("mini.ai").setup({
+    require("mini.ai").setup {
       custom_textobjects = {
-        l = spec_treesitter({
+        F = spec_treesitter {
           a = { "@conditional.outer", "@loop.outer" },
           i = { "@conditional.inner", "@loop.inner" },
-        }),
-        h = spec_treesitter({
+        },
+        h = spec_treesitter {
           a = { "@attribute.outer" },
           i = { "@attribute.inner" },
-        }),
+        },
       },
-    })
+    }
   end,
   function()
     require("mini.files").setup()
