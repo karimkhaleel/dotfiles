b140519c6631c63f889fe52be0d52e5fe6364026
Karim Khaleel <code.karim@gmail.com>
2025-08-17 12:00:56 -0400

    fix(nvim): update venv selector setup

--- dot_config/nvim/lua/plugins/python.lua
+++ dot_config/nvim/lua/plugins/python.lua
@@ -5,14 +5,11 @@ return {
     dependencies = {
       "neovim/nvim-lspconfig",
       "mfussenegger/nvim-dap",
-      "mfussenegger/nvim-dap-python",
+      "mfussenegger/nvim-dap-python", --optional
       { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
     },
     lazy = false,
     branch = "regexp", -- This is the regexp branch, use this for the new version
-    config = function()
-      require("venv-selector").setup()
-    end,
     keys = {
       -- Keymap to open VenvSelector to pick a venv.
       { "<leader>tvs", "<cmd>VenvSelect<cr>" },
