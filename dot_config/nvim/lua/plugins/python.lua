ed0748d980630aa4c0ed34b8bbf4127c66958280
Karim Khaleel <code.karim@gmail.com>
2025-10-28 00:27:24 -0400

    fix(nvim): update venv selector to main branch

--- dot_config/nvim/lua/plugins/python.lua
+++ dot_config/nvim/lua/plugins/python.lua
@@ -1,20 +1,20 @@
 return {
   {
-
     "linux-cultist/venv-selector.nvim",
     dependencies = {
       "neovim/nvim-lspconfig",
-      "mfussenegger/nvim-dap",
-      "mfussenegger/nvim-dap-python", --optional
-      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } }, -- optional: you can also use fzf-lua, snacks, mini-pick instead.
     },
-    lazy = false,
-    branch = "regexp", -- This is the regexp branch, use this for the new version
+    ft = "python", -- Load when opening Python files
     keys = {
       -- Keymap to open VenvSelector to pick a venv.
       { "<leader>tvs", "<cmd>VenvSelect<cr>" },
       -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
       { "<leader>tvc", "<cmd>VenvSelectCached<cr>" },
+    },
+    opts = { -- this can be an empty lua table - just showing below for clarity.
+      search = {}, -- if you add your own searches, they go here.
+      options = {}, -- if you add plugin options, they go here.
     },
   },
 
