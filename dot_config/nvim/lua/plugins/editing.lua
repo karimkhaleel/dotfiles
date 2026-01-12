bbd4938396eaa396b08b25db6d35dd2f6a4b2d51
Karim Khaleel <code.karim@gmail.com>
2026-01-12 12:42:03 -0500

    fix(neovim): add back autopairs

--- dot_config/nvim/lua/plugins/editing.lua
+++ dot_config/nvim/lua/plugins/editing.lua
@@ -1,5 +1,12 @@
 return {
   {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+  },
+  {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
